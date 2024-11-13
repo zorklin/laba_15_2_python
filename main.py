da4f1cf4a63acfc19b2b446d8eb934862076abf9
zorklin <well.well.well.pink@gmail.com>
2024-11-13 20:21:43 +0200

    update

--- main.py
+++ main.py
@@ -6,22 +6,36 @@ file_name = "data.csv"
 try:
     df = pd.read_csv(file_name)
 except FileNotFoundError:
-    print(f"error, file {file_name} not found")
+    print(f"error file {file_name} not found")
     exit()
 except pd.errors.EmptyDataError:
-    print("error, file is empty")
+    print("error file is empty")
     exit()
 except pd.errors.ParserError:
-    print("error parser")
+    print("error parser error")
     exit()
+
 
 df["Date"] = pd.to_datetime(df["Date"])
 df["Month"] = df["Date"].dt.month
-df["Total_Cyclists"] = df.iloc[:, 1:-1].sum(axis = 1)
 
+street_columns = df.columns[1:-2]
+print("Available streets:")
+for street in street_columns:
+    print(street)
+
+
+selected_street = input("Enter street: ").strip()
+
+if selected_street not in df.columns:
+    print(f"Error column {selected_street} not found")
+    exit()
+
+df["Total_Cyclists"] = df[selected_street]
 monthly_totals = df.groupby("Month")["Total_Cyclists"].sum()
 most_popular_month = monthly_totals.idxmax()
 max_cyclists = monthly_totals.max()
+
 
 month_names = {
     1: "January",
@@ -42,13 +56,12 @@ most_popular_month_name = month_names[most_popular_month]
 print(f"The most popular month: {most_popular_month_name}")
 print(f"Number of cyclists in this month: {max_cyclists}")
 
-street_columns = df.columns[1:-2]
-plt.figure(figsize = (15, 8))
-for street in street_columns:
-    daily_totals_street = df.set_index("Date")[street]
-    plt.plot(daily_totals_street.index, daily_totals_street.values, label = street)
 
-plt.title("The number of cyclists by day for various streets in 2013")
+plt.figure(figsize = (15, 8))
+daily_totals_street = df.set_index("Date")[selected_street]
+plt.plot(daily_totals_street.index, daily_totals_street.values, label = selected_street)
+
+plt.title(f"The number of cyclists for {selected_street} in 2013")
 plt.xlabel("Date")
 plt.ylabel("The number of cyclists")
 plt.legend(loc = "upper left")
